d5479304032427ec15ca77a8f67a447873e2cb25
trist-in <tristinfranklinbiz@gmail.com>
2025-11-19 16:30:56 -0500

    fixed login

--- backend/user-authentication/controllers/authenticationController.js
+++ backend/user-authentication/controllers/authenticationController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const db = require("../controllers/authenticationController");
+const db = require("../models/authenticationModel");
 
 require("dotenv").config();
 
--- backend/user-authentication/routes/authenticationRoutes.js
+++ backend/user-authentication/routes/authenticationRoutes.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require("express");
 //Creates a router
 const router = express.Router();
 //Imports functions to handle register, login, and logout from authenticationController.js
-const { register, login, logout } = require('../controllers/authenticationController');
+const {
+  register,
+  login,
+  logout,
+} = require("../controllers/authenticationController");
 //Imports verifyToken from authenticationMiddleware.js
-const verifyToken = require('../middleware/authenticationMiddleware');
+const verifyToken = require("../middleware/authenticationMiddleware");
 
 //Handles registration of new users using the registerUser api
-router.post('/register', register);
+router.post("/register", register);
 
 //Handles login of users using the userLogin api
-router.post('/login', login);
+router.post("/login", login);
 
 //Handles the logout of users using the userLogout api
-router.post('/logout', logout);
+router.post("/logout", logout);
 
 //Handles the verification of tokens using the accessProtectedRoutes api
-router.get('/protected', verifyToken, (request, response) => {response.json({ message: `Welcome ${request.user.email}!` });});
+router.get("/protected", verifyToken, (request, response) => {
+  response.json({ message: `Welcome ${request.user.email}!` });
+});
 
 module.exports = router;
