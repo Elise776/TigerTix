e6e076036084de7277fffb796b464ed96ff94c4c
Elise776 <elise7@clemson.edu>
2025-11-23 16:35:28 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes/routes");
-app.use(cors());
 app.use("/api", routes);
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://tiger-tix-nine.vercel.app"
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true
+  })
+);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running at
